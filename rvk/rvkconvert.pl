51eab5f054d6830d1ddd247794dba6b656ae1fcd
Jakob Voss <voss@gbv.de>
2016-03-02 11:03:29 +0100

    Cleanup and extend RVK conversion code

--- rvk/rvkconvert.pl
+++ rvk/rvkconvert.pl
@@ -1,86 +1,121 @@
 #!/usr/bin/perl
+use v5.14;
 
-use strict;
-use warnings;
+=head1 DESCRIPTION
+
+Conversion script from RVK-XML dump to JSKOS.
+
+Output file is newline-delimited JSON with one JSKOS Concept on each line.
+
+=cut
 
 use XML::LibXML;
 use JSON;
 use Data::Dumper;
-use Path::Class;
+use URI::Escape;
 
-my $filename = 'rvko_2015_4.xml';
-my $json = 'rvk_2015_4.json';
-my $conv = JSON->new->utf8;
-my $dump = file('dump.json');
-my $dout = $dump->openw();
+my $xmlfile   = 'rvko_2015_4.xml';    # input file
+my $jskosfile = 'rvk_2015_4.json';    # output file
 
-if (-e $json) {
-  unlink $json;
-} 
+my $rvkuri   = 'http://bartoc.org/en/node/533';
+my $urispace = 'http://uri.gbv.de/terminology/rvk/';
 
+my $JSON   = JSON->new->utf8->canonical;
 my $parser = XML::LibXML->new();
-my $doc    = $parser->parse_file($filename);
-my $c = 0;
+my $doc    = $parser->parse_file($xmlfile);
+my $count  = 0;
 
-foreach my $node ($doc->findnodes('//node')) {
-  my %new;
-  $new{'prefLabel'}{'de'} = $node->getAttribute('benennung');
-  $new{'notation'}[0] = $node->getAttribute('notation');
-  $new{'inScheme'}[0]{'notation'}[0] = 'RVK';
-  $new{'inScheme'}[0]{'uri'} = "http://d-nb.info/gnd/4449787-8";
-  my($children) = $node->findnodes('./children');
-  if($children){
-    $new{'narrower'} = [];
-    foreach my $child ($children->findnodes('./node')){
-      my($childNotation) = $child->getAttribute('notation');
-      my($childLabel) = $child->getAttribute('benennung');
-      push $new{'narrower'}, { notation => $childNotation, prefLabel => { de => $childLabel } };
+open( my $out, '>', $jskosfile ) or die "Failed to open $jskosfile for writing";
+
+sub conceptURI {
+    my $notation = shift;
+    $notation =~ s/\s//g;
+    return $urispace . uri_escape($notation);
+}
+
+sub conceptType {
+    my $notation = shift;
+
+    # TODO: notation ranges such as "FX 403998 - FX 404305" may need to have another type
+    my $type = ['http://www.w3.org/2004/02/skos/core#Concept'];
+
+    return $type;
+}
+
+foreach my $node ( $doc->findnodes('//node') ) {
+    my $notation = $node->getAttribute('notation');
+    my %jskos    = (
+        uri       => conceptURI($notation),
+        type      => conceptType($notation),
+        notation  => [ $notation ],
+        preflabel => {
+            de => $node->getAttribute('benennung'),
+        },
+        inScheme => {
+            uri      => $rvkuri,
+            notation => ['RVK'],
+        }
+    );
+    my ($children) = $node->findnodes('./children');
+    if ($children) {
+        $jskos{'narrower'} = [];
+        foreach my $child ( $children->findnodes('./node') ) {
+            my ($childNotation) = $child->getAttribute('notation');
+            my ($childLabel)    = $child->getAttribute('benennung');
+            push @{$jskos{'narrower'}},
+              {
+                uri       => conceptURI($childNotation),
+                type      => conceptType($childNotation),
+                notation  => [ $childNotation ],
+                prefLabel => { de => $childLabel }
+              };
+        }
     }
-  }
-  
-  my($notes) = $node->findnodes('./content');
-  my $bemerkung;
-  if($notes){
-    $bemerkung = $notes->getAttribute('bemerkung');
-  }
-  my $reg;
-  my($register) = $node->findnodes('./register');
-  if($register){
-    $reg = "Register:";
-    foreach my $entry ($node->findnodes('./register')){
-      my $add = $entry->textContent;
-      $add =~ s/\s+$//;
-#       print Dumper($add);
-      $reg .= " $add.";
-      
+
+    my ($notes) = $node->findnodes('./content');
+    my $bemerkung;
+    if ($notes) {
+        $bemerkung = $notes->getAttribute('bemerkung');
     }
-  }
-  if($notes || $register){
-    $new{'scopeNote'}{'de'} = [];
-    if($notes){
-      push $new{'scopeNote'}{'de'}, $bemerkung;
+    my $reg;
+    my ($register) = $node->findnodes('./register');
+    if ($register) {
+        $reg = "Register:";
+        foreach my $entry ( $node->findnodes('./register') ) {
+            my $add = $entry->textContent;
+            $add =~ s/\s+$//;
+            $reg .= " $add.";
+
+        }
     }
-    if($register){
-      push $new{'scopeNote'}{'de'}, $reg;
+    if ( $notes || $register ) {
+        $jskos{'scopeNote'}{'de'} = [];
+        if ($notes) {
+            push @{$jskos{'scopeNote'}{'de'}}, $bemerkung;
+        }
+        if ($register) {
+            push @{$jskos{'scopeNote'}{'de'}}, $reg;
+        }
     }
-  }
-  
-  my $not = $new{'notation'}[0];
-  my $parentC = $node->parentNode;
-  if($parentC->nodeName eq 'children'){
-    my $parent = $parentC->parentNode;
-    $new{'broader'} = [];
-    my $broaderNot = $parent->getAttribute('notation');
-    my $broaderLabel = $parent->getAttribute('benennung');
-    push $new{'broader'}, { notation => $broaderNot, prefLabel => { de => $broaderLabel } };
-  }
-  my $enc_json = $conv->pretty->encode(\%new);
-  open(my $js, '>>', $json) or die "No output file!";
-  print $js $enc_json;
-  close $js;
-  $c++;
-  if($c % 1000 == 0){
-    print "$c nodes processed!\n";
-  }
+
+    my $parentC = $node->parentNode;
+    if ( $parentC->nodeName eq 'children' ) {
+        my $parent = $parentC->parentNode;
+        $jskos{'broader'} = [];
+        my $parentNotation   = $parent->getAttribute('notation');
+        my $parentLabel      = $parent->getAttribute('benennung');
+        push @{$jskos{'broader'}},
+          { 
+              uri       => conceptURI($parentNotation),
+              type      => conceptType($parentNotation),
+              notation  => $parentNotation, 
+              prefLabel => { de => $parentLabel } 
+          };
+    }
+    say $out $JSON->encode( \%jskos );
+    $count++;
+    if ( $count % 1000 == 0 ) {
+        say "$count nodes processed.\t$notation";
+    }
 }
 
