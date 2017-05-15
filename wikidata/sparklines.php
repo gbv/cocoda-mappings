ae6300cc1d2a2f374882eaa574a79d86f401797d
Jakob Voss <voss@gbv.de>
2017-05-15 22:23:42 +0200

    Use variable sparkline width

--- wikidata/sparklines.php
+++ wikidata/sparklines.php
@@ -7,7 +7,7 @@ use Davaxi\Sparkline;
 $data = [];
 
 foreach (file('statlog.csv') as $line) {
-    list($date,$count,$prop) = explode(',',trim($line));
+    list($date,$prop,$count) = explode(',',trim($line));
     $data[$prop][$date] = $count;
 }
 
@@ -19,23 +19,26 @@ foreach (file('total.csv') as $line) {
 # make sure all properties have same dates
 foreach ($data as $prop => $counts) {
     foreach ($counts as $date => $value) {
-        $dates[$date] = $date;
+#        $dates[$date] = $date;
         @$data['kos'][$date]++;
     }
 }
 
+/*
 foreach ($data as $prop => $counts) {
     foreach ($dates as $date) {
         if (!isset($counts[$date])) {
-            $counts[$date] = 0;
+#            $counts[$date] = 0;
         }
     }
     ksort($counts);
     $data[$prop] = $counts; 
 } 
+ */
 
 foreach ($data as $prop => $counts) {
     $sparkline = new Sparkline();
+    echo "$prop:".implode(",",$counts)."\n";    
     $min = min($counts);
     $max = max($counts);
     if ($min == $max and $min > 0) {
@@ -45,6 +48,11 @@ foreach ($data as $prop => $counts) {
         function($count) use ($min) { return $count-$min; }, 
         $counts
     );
+    $width = 80;
+    if (count($counts) < 40) {
+        $width = max([2*count($counts),10]);
+    }
+    $sparkline->setWidth($width);
     $sparkline->setData($counts);
 
     $sparkline->save("$prop.png");
