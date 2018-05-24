6997549cbe360a43eedfe8eec973d9c3e5a6e900
Jakob Voss <voss@gbv.de>
2018-05-24 15:09:37 +0200

    Add basic occurrences API

--- kos.php
+++ kos.php
@@ -1,7 +1,7 @@
 <?php
 
 foreach (file(dirname(__FILE__).'/csv/kos.tsv') as $line) {
-    list ($notation, $uri) = explode("\t",trim($line));
+    list ($notation, $uri, $pattern) = explode("\t",trim($line));
     if (!isset($kos)) { 
         $kos = [];
         continue; 
@@ -9,7 +9,8 @@ foreach (file(dirname(__FILE__).'/csv/kos.tsv') as $line) {
     $notation = strtoupper($notation);
     $kos[$notation] = [
         'notation' => [$notation],
-        'uri' => $uri
+        'uri' => $uri,
+        'PATTERN' => $pattern
     ];
 }
 
