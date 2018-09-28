f1ae98b7114ef6856c94f0d631a8cadeecbeeb54
Jakob Voss <voss@gbv.de>
2018-09-28 08:37:37 +0200

    Extend index intro text

--- index.php
+++ index.php
@@ -3,9 +3,14 @@ $BASE = '..';
 include "$BASE/header.php";
 ?>
 
-<p>We collect concordances and mappings in a public database.</p>
+<p>
+  We collect concordances and mappings in a public database run with
+  <a href="https://github.com/gbv/jskos-server">jskos-server</a>. The
+  database can be queried via this web interface and via an API at 
+  <a href="https://coli-conc.gbv.de/api/">https://coli-conc.gbv.de/api/</a>. 
+</p>
 
-<div id=mappingsApp></div>
+<div id="mappingsApp"></div>
 
 <?php
 $html = (file_exists('../cocoda/mappings.html')
