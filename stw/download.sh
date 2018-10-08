25f895b208db601eeebb8b0ed412df3a914a2ab5
Jakob Voss <voss@gbv.de>
2018-10-08 15:23:33 +0200

    Update STW-GND mappings (#22)

---
+++ stw/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+FILE=stw_gnd_mapping.nt.zip
+wget -O $FILE http://zbw.eu/stw/version/9.0/download/$FILE
---
+++ stw/process.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+use v5.14;
+
+# convert ZBW STW mappings from NTriples to CSV
+#
+# $ wget http://zbw.eu/stw/version/9.0/download/stw_gnd_mapping.nt.zip
+# $ zcat stw_gnd_mapping.nt.zip | ./process.pl > stw_gnd_zbw.csv
+
+say "fromNotation;toNotation;type";
+while (<>) {
+    say "$1;$3;$2" if $_ =~ qr{^
+        <http://zbw\.eu/stw/descriptor/([^>]+)>\s+
+        <http://www\.w3\.org/2004/02/skos/core\#(broad|narrow|exact|close|related)Match>\s+
+        <http://d-nb\.info/gnd/([^>]+)>
+    }x;
+}
