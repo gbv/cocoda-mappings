921b8fdb1f3c6ea54e8d1ed9f69fa619dd20b2c9
Stefan Peters <Stefan.Peters@gbv.de>
2019-01-03 12:27:29 +0100

    Update index.php with new URL to Cocoda instance

--- index.php
+++ index.php
@@ -13,18 +13,18 @@ include "$BASE/header.php";
 <div id="mappingsApp"></div>
 
 <?php
-$html = (file_exists('../cocoda/mappings.html')
-      ? file_get_contents('../cocoda/mappings.html')
-      : file_get_contents('http://coli-conc.gbv.de/cocoda/mappings.html')) ?? '';
+$html = (file_exists('../cocoda/app/mappings.html')
+      ? file_get_contents('../cocoda/app/mappings.html')
+      : file_get_contents('http://coli-conc.gbv.de/cocoda/app/mappings.html')) ?? '';
 if (preg_match_all('!src=["\']?([^"\'>]+)!', $html, $match)) {
   foreach ($match[1] as $src) {
-      echo "<script type='text/javascript' src='../cocoda/$src'></script>";
+      echo "<script type='text/javascript' src='../cocoda/app/$src'></script>";
   }
 }
 if (preg_match_all('!link href=["\']?([^"\'> ]+)!', $html, $match)) {
   foreach ($match[1] as $src) {
     if (strpos($src, "bootstrap") === false) {
-      echo "<link href='../cocoda/$src' rel='stylesheet'>";
+      echo "<link href='../cocoda/app/$src' rel='stylesheet'>";
     }
   }
 }
