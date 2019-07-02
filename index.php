353ab8eb2ed86e8de7a5b7ba3855a4d9f40ec7be
Jakob Voss <voss@gbv.de>
2019-07-02 11:01:58 +0200

    Replace mapping app by simple table

--- index.php
+++ index.php
@@ -1,32 +1,50 @@
 <?php
 $BASE = '..';
+$TITLE = 'Concordance Registry';
+$FORMAT = 'markdown';
+require_once "$BASE/vendor/autoload.php";
 include "$BASE/header.php";
 ?>
 
-<p>
-  We collect concordances and mappings in a public database run with
-  <a href="https://github.com/gbv/jskos-server">jskos-server</a>. The
-  database can be queried via this web interface and via an API at 
-  <a href="https://coli-conc.gbv.de/api/">https://coli-conc.gbv.de/api/</a>. 
-</p>
+We collect concordances and mappings in a public database run with
+[jskos-server](https://github.com/gbv/jskos-server). The database can be
+queried via this web interface and via an API at
+<https://coli-conc.gbv.de/api/>. 
 
-<div id="mappingsApp"></div>
+🡒 [See all concordances in Cocoda](https://coli-conc.gbv.de/cocoda/app/?concordances).
 
-<?php
-$html = (file_exists('../cocoda/app/mappings.html')
-      ? file_get_contents('../cocoda/app/mappings.html')
-      : file_get_contents('http://coli-conc.gbv.de/cocoda/app/mappings.html')) ?? '';
-if (preg_match_all('!src=["\']?([^"\'>]+)!', $html, $match)) {
-  foreach ($match[1] as $src) {
-      echo "<script type='text/javascript' src='../cocoda/app/$src'></script>";
-  }
-}
-if (preg_match_all('!link href=["\']?([^"\'> ]+)!', $html, $match)) {
-  foreach ($match[1] as $src) {
-    if (strpos($src, "bootstrap") === false) {
-      echo "<link href='../cocoda/app/$src' rel='stylesheet'>";
-    }
-  }
-}
+<table class="table table-sm" id="release-table">
+ <tr>
+   <th>from</th>
+   <th>to</th>
+   <th>description</th>
+   <th>creator</th>
+   <th>date</th>
+   <th style="text-align: right">mappings</th>
+   <!--th>download</th-->
+   <th></th>
+ </tr>
+</table>
 
-include "$BASE/footer.php";
+
+<script type="text/javascript">
+$(document).ready(function(){
+  $.getJSON('https://coli-conc.gbv.de/api/concordances', function(list) {
+    list.forEach( function(conc) {
+      var row = $('<tr>')
+      // TODO: use utility method to safe deep object access
+      row.append('<td>'+conc.fromScheme.notation[0]+'</td>')
+      row.append('<td>'+conc.toScheme.notation[0]+'</td>')
+      row.append('<td>'+conc.scopeNote.de[0]+'</td>')
+      row.append('<td>'+conc.creator[0].prefLabel.de+'</td>')
+      row.append('<td>'+(conc.created || '')+'</td>')
+      row.append('<td align="right">'+(conc.extent || '')+'</td>')      
+      // TODO: get downloads from conc.distributions
+      var url = "https://coli-conc.gbv.de/cocoda/app/?search=" 
+      + encodeURI(JSON.stringify({partOf: conc.uri}))
+      row.append('<td>🡒 <a href="'+url+'">Cocoda</a></td>')
+      $('#release-table').append(row)
+    })
+  })
+})
+</script>
